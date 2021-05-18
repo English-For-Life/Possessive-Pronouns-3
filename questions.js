b566b8ba8782050a1722d6db7ab3686aab28d06f
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:07:53 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"This bird has broken ___ wings.",
+        options:["it", "its", "hers", "her"],
+        answer: 1
+    },
+    {
+     q:"___ task is easier than mine.",
+        options:["Your", "Yours", "Mine", "My"],
+        answer: 0   
+    },
+    {
+        q:"___ dress is cheaper than hers.",
+        options:["I", "Me", "Mine", "My"],
+        answer: 3
+    },
+    {
+        q:"Meet Ann. She is a friend of ___ .",
+        options:["your", "me", "my", "mine"],
+        answer: 3
+    },
+    {
+        q:"I don't believe ___ stories.",
+        options:["ours", "hers", "his", "theirs"],
+        answer: 2
+    },
+    {
+        q:"My husband and I realy like ___ new flat. It has a lot of space.",
+        options:["ours", "our", "yours", "theirs"],
+        answer: 1
+    },
+    {
+        q:"Is this phone his or ___ .",
+        options:["hers", "his", "my", "its"],
+        answer: 0
+    },
+    {
+        q:"Nick has two sisters. ___ names are Lucy and Jane.",
+        options:["Ours", "They", "Theirs", "Their"],
+        answer: 3
+    },
+    {
+        q:"My hobby is swimming. What's ___ ?",
+        options:["your", "yours", "you", "her"],
+        answer: 1
+    },
+    {
+        q:"I wash ___ car every Saturday.",
+        options:["our", "ours", "my", "mine"],
+        answer: 2
+    }
+
+]
